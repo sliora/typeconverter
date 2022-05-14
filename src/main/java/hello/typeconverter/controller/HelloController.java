f5351066dac6a80f9e8cc7054d42cf4994bcf57c
zerom-H <cupdisin@gmail.com>
2022-05-14 23:41:47 +0900

    타입컨버터 들어가기전 서블릿과 스프링 비교

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer integer = Integer.valueOf(data);
+        System.out.println("integer = " + integer);
+
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
